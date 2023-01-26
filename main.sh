49cdd19f1a90e75815e4c62238c1145b308260c9
liotium96 <123634621+liotium96@users.noreply.github.com>
2023-01-26 05:06:44 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 # Set this to 1 to automatically attempt a fix when an error occurs while running npm start.
 fix=1
 
+python3 /app/initializ.py &
 
 # Stuff to do at first run(submodule, npm install).
 if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
